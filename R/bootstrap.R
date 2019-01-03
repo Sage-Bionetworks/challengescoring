fb2a2b4fab9a704001fbcea16a7a2328962efccc
robert allaway <robert.j.allaway@gmail.com>
2019-01-03 15:20:58 -0800

    adding comments
    adding some comments to the functions

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,57 +1,7 @@
 #' @importFrom magrittr %>%
 #' @importFrom plyr aaply
 #' @importFrom plyr alply
-
-
-indexedScore <- function(dataIndices, goldStandardMatrix, predictionsMatrix, scoreFun){
-  gold <- goldStandardMatrix[dataIndices,]
-  if(ncol(predictionsMatrix)>1){
-    plyr::aaply(predictionsMatrix[dataIndices,], 2, scoreFun, gold = gold)
-  }else{
-    scoreFun(gold = goldStandardMatrix[dataIndices,], pred = predictionsMatrix[dataIndices,])
-  }
-}
-
-bootstrappingMetric <- function(goldStandardMatrix, predictionsMatrix, scoreFun = scoreFun, bootstrapN = bootstrapN, seed = seed, doParallel = T, ...)
-{
-  # matrix, columns are boostraps, rows are samples
-
-  bsIndexMatrix <- matrix(1:nrow(goldStandardMatrix), nrow(goldStandardMatrix), bootstrapN)
-  bsIndexMatrix <- t(aaply(bsIndexMatrix, 2, sample, replace = T))# bootstrap indices
-  doMC::registerDoMC(cores = detectCores()-1)
-
-  bsMetric  <- alply(.data = bsIndexMatrix,
-                      .margins = 2,
-                      .fun = indexedScore,
-                      .parallel = doParallel,
-                      goldStandardMatrix,
-                      predictionsMatrix,
-                      scoreFun,
-                      ...)
-
-
-    # matrix, columns are teams, rows are bootstraps
-
-  bsMetric <- do.call(rbind, bsMetric)
-
-  return(bsMetric)
-}
-
-computeBayesFactor <- function(bootstrapMetricMatrix, bestTeamIndex, largerIsBetter = TRUE)
-{
-  if(largerIsBetter==TRUE){
-    M <- as.data.frame(bootstrapMetricMatrix - bootstrapMetricMatrix[,bestTeamIndex])
-    K <- apply(M ,2, function(x) {sum(x <= 0)/sum(x > 0)})
-    K[bestTeamIndex] <- 0
-    return(K)
-  }else{
-    M <- as.data.frame(bootstrapMetricMatrix - (bootstrapMetricMatrix[,bestTeamIndex]))
-    K <- apply(M ,2, function(x) {sum(-x <= 0)/sum(-x > 0)})
-    K[bestTeamIndex] <- 0
-    return(K)
-  }
-}
-
+#'
 #' Calculate a bootstrapped score for an initial submission or subsequent submission.
 #'
 #' @param predictionsPath The relative path to the current prediction csv.
@@ -84,19 +34,20 @@ bootLadderBoot <- function(predictionsPath,
                            doParallel = TRUE){
 
   if(verbose == TRUE){print("reading gold standard file")}
-  goldStandardDF <- read.csv(goldStandardPath)
+  goldStandardDF <- read.csv(goldStandardPath) ##reads the gold standard file
 
   if(verbose == TRUE){print("reading prediction file")}
-  predictionsDF <- read.csv(predictionsPath)
+  predictionsDF <- read.csv(predictionsPath) ## reads the prediction file
 
-  if(is.null(prevPredictionsPath)){
-  joinedData <- dplyr::full_join(goldStandardDF, predictionsDF) %>%
-    dplyr::select_(goldStandardColname, predictionColname) %>%
-    purrr::set_names('gold', 'pred')
+  if(is.null(prevPredictionsPath)){ ## tests for previous submission -if none, just joins gold standard and predicition into one dataframe (ensures matched order on id columns)
+    joinedData <- dplyr::full_join(goldStandardDF, predictionsDF) %>%
+      dplyr::select_(goldStandardColname, predictionColname) %>%
+      purrr::set_names('gold', 'pred')
 
-  goldStandardMatrix <- joinedData[,1, drop = FALSE] %>% as.matrix()
-  predictionsMatrix <- joinedData[,2, drop = FALSE] %>% as.matrix()
-  }else{
+    goldStandardMatrix <- joinedData[,1, drop = FALSE] %>% as.matrix() #make a gold standard matrix (1 column)
+    predictionsMatrix <- joinedData[,2, drop = FALSE] %>% as.matrix() #make a prediction data matrix (1 column)
+
+  }else{ ## if there is a previous submission, that gets read in also, and joined to this dataframe (ensures matched order on id columns)
     if(verbose == TRUE){print("reading previous prediction file")}
     prevPredictionsDF <- read.csv(predictionsPath) %>% dplyr::mutate_("prevpred"=predictionColname) %>% dplyr::select(-predictionColname)
     joinedData <- dplyr::full_join(goldStandardDF, predictionsDF) %>%
@@ -104,42 +55,92 @@ bootLadderBoot <- function(predictionsPath,
       dplyr::select_(goldStandardColname, predictionColname, "prevpred") %>%
       purrr::set_names('gold', 'pred', 'prevpred')
 
-    goldStandardMatrix <- joinedData[,1, drop = FALSE] %>% as.matrix()
-    predictionsMatrix <- joinedData[,2:3, drop = FALSE] %>% as.matrix()
+    goldStandardMatrix <- joinedData[,1, drop = FALSE] %>% as.matrix() #make a gold standard matrix (1 column)
+    predictionsMatrix <- joinedData[,2:3, drop = FALSE] %>% as.matrix() #make a prediction matrix (2 columns - current prediction, previous prediction)
   }
 
+  ## bootstrap gold standard and predictions
   bootstrapMetricMatrix <- bootstrappingMetric(goldStandardMatrix = goldStandardMatrix,
-                                                     predictionsMatrix = predictionsMatrix,
-                                                     scoreFun = scoreFun,
-                                                     bootstrapN = bootstrapN,
-                                                     seed = seed,
-                                                     doParallel = doParallel)
+                                               predictionsMatrix = predictionsMatrix,
+                                               scoreFun = scoreFun,
+                                               bootstrapN = bootstrapN,
+                                               seed = seed,
+                                               doParallel = doParallel)
 
   if(verbose == TRUE){print("joining bootstrapped data frames")}
-  if(!is.null(prevPredictionsPath) & largerIsBetter == TRUE){
-    K <- computeBayesFactor(bootstrapMetricMatrix, 1, largerIsBetter = TRUE)
+
+  if(!is.null(prevPredictionsPath) & largerIsBetter == TRUE){ #test for previous prediction data and whether larger scores are better
+
+    K <- computeBayesFactor(bootstrapMetricMatrix, 1, largerIsBetter = TRUE) #compute bayes factor where a larger score is better
     meanBS_new <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
     meanBS_prev <- mean(bootstrapMetricMatrix[1:reportBootstrapN,2])
-    if(K['pred'] > bayesThreshold & meanBS_new > meanBS_prev){
+    if(K['pred'] > bayesThreshold & meanBS_new > meanBS_prev){ ##if bayes score is greater than threshold set by user, AND score is better, report bootstrapped score
       returnedScore <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
     }else{
-      returnedScore <- NA
+      returnedScore <- NA ##if within K threshold, return NA for score
     }
-  }else if(!is.null(prevPredictionsPath) & largerIsBetter == FALSE){
-      K <- computeBayesFactor(bootstrapMetricMatrix, 1, largerIsBetter = FALSE)
-      meanBS_new <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
-      meanBS_prev <- mean(bootstrapMetricMatrix[1:reportBootstrapN,2])
-      if(K['pred'] > bayesThreshold & meanBS_new < meanBS_prev){
-        returnedScore <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
-      }else{
-        returnedScore <- NA
-      }
-  }else if(is.null(prevPredictionsPath)){
+  }else if(!is.null(prevPredictionsPath) & largerIsBetter == FALSE){ #compute bayes factor where a smaller score is better
+    K <- computeBayesFactor(bootstrapMetricMatrix, 1, largerIsBetter = FALSE)
+    meanBS_new <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
+    meanBS_prev <- mean(bootstrapMetricMatrix[1:reportBootstrapN,2])
+    if(K['pred'] > bayesThreshold & meanBS_new < meanBS_prev){ ##if bayes score is greater than threshold set by user, AND score is better, report bootstrapped score
+      returnedScore <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
+    }else{
+      returnedScore <- NA ##if within K threshold, return NA for score
+    }
+  }else if(is.null(prevPredictionsPath)){ ## if there is no previous file, simply return bootstrapped score
     if(verbose == TRUE){print("no previous file")}
     returnedScore <- mean(bootstrapMetricMatrix[1:reportBootstrapN,1])
   }
   return(returnedScore)
 }
 
+# this function creates paired bootstrap indices and then scores the bootstrapped data using the indexedScore function
+bootstrappingMetric <- function(goldStandardMatrix, predictionsMatrix, scoreFun = scoreFun, bootstrapN = bootstrapN, seed = seed, doParallel = T, ...){
 
+   # matrix, columns are boostraps, rows are samples
+  bsIndexMatrix <- matrix(1:nrow(goldStandardMatrix), nrow(goldStandardMatrix), bootstrapN)
+  bsIndexMatrix <- t(aaply(bsIndexMatrix, 2, sample, replace = T))# create bootstrap indices
+  doMC::registerDoMC(cores = detectCores()-1)
+
+  bsMetric  <- alply(.data = bsIndexMatrix, ##score bootstrapped indices
+                      .margins = 2,
+                      .fun = indexedScore,
+                      .parallel = doParallel,
+                      goldStandardMatrix,
+                      predictionsMatrix,
+                      scoreFun,
+                      ...)
+
+  # matrix, columns are prediction sets, rows are bootstraps
+  bsMetric <- do.call(rbind, bsMetric)
+  return(bsMetric)
+}
+
+#calculate bayes factor from a set of bootstrapped scores
+computeBayesFactor <- function(bootstrapMetricMatrix, bestTeamIndex, largerIsBetter = TRUE){
+  if(largerIsBetter==TRUE){
+    M <- as.data.frame(bootstrapMetricMatrix - bootstrapMetricMatrix[,bestTeamIndex])
+    K <- apply(M ,2, function(x) {sum(x <= 0)/sum(x > 0)})
+    K[bestTeamIndex] <- 0
+    return(K)
+  }else{
+    M <- as.data.frame(bootstrapMetricMatrix - (bootstrapMetricMatrix[,bestTeamIndex]))
+    K <- apply(M ,2, function(x) {sum(-x <= 0)/sum(-x > 0)})
+    K[bestTeamIndex] <- 0
+    return(K)
+  }
+}
+
+#'wrapper function to pass bootstrapped data to scoring function provided by user
+#'this allows user to provide a simple scoring function of the form function(gold, pred)
+#'where gold and pred are vectors with the gold standard data and the prediction data
+indexedScore <- function(dataIndices, goldStandardMatrix, predictionsMatrix, scoreFun){
+  gold <- goldStandardMatrix[dataIndices,]
+  if(ncol(predictionsMatrix)>1){
+    plyr::aaply(predictionsMatrix[dataIndices,], 2, scoreFun, gold = gold)
+  }else{
+    scoreFun(gold = goldStandardMatrix[dataIndices,], pred = predictionsMatrix[dataIndices,])
+  }
+}
 
